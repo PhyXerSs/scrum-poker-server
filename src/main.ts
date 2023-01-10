3f16a1a2e446b1029c9b8a9bafd0fc79892c5153
PhyXerSs <kuntaice28@gmail.com>
2023-01-11 02:30:59 +0700

    change cors option

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({limit: '50mb'}));
   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-  const options = {
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204,
-    "credentials":true
-  }
-  app.enableCors(options)
+  // const options = {
+  //   "origin": "*",
+  //   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  //   "preflightContinue": false,
+  //   "optionsSuccessStatus": 204,
+  //   "credentials":true
+  // }
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
