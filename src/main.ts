4f371b4957d89d6529c094ac1f1b6acfa111cdec
PhyXerSs <kuntaice28@gmail.com>
2023-01-11 02:26:49 +0700

    change cors option

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({limit: '50mb'}));
   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-  // app.enableCors();
+  const options = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "credentials":true
+  }
+  app.enableCors(options)
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
